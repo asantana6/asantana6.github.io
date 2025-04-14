be8f7d09e9b6b65fb18e7631da6cd2b594345c1a
Abdiel <asantan6@charlotte.edu>
2025-04-14 19:49:16 -0400

    sdfg

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,38 +1,74 @@
+$(document).ready(function() {
+    // 1. First declare all variables
+    let slideIndex = 1;
+    let slideInterval;
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    // 2. Define all helper functions first
+    function showSlides(n) {
+        const slides = $(".mySlides");
+        const dots = $(".dot");
+        
+        // Handle wrap-around
+        if (n > slides.length) { slideIndex = 1; }
+        if (n < 1) { slideIndex = slides.length; }
+        
+        // Hide all slides
+        slides.hide();
+        
+        // Remove active class from all dots
+        dots.removeClass("active");
+        
+        // Show current slide and activate corresponding dot
+        slides.eq(slideIndex-1).fadeIn(1000);
+        dots.eq(slideIndex-1).addClass("active");
     }
-    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-}
 
-// Auto-advance slides every 5 seconds
-setInterval(function() {
-    plusSlides(1);
-}, 5000);
+    function startTimer() {
+        slideInterval = setInterval(function() {
+         //   plusSlides(1);
+        }, 5000000);
+    }
+
+    function resetTimer() {
+        clearInterval(slideInterval);
+        startTimer();
+    }
+
+    function plusSlides(n) {
+        resetTimer();
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        resetTimer();
+        showSlides(slideIndex = n);
+    }
+
+    // 3. Only after all functions are defined do we:
+    function initSlideshow() {
+        // Initialize display
+        showSlides(slideIndex);
+        startTimer();
+        
+        // Set up event handlers
+        $(".prev").click(function() { plusSlides(-1); });
+        $(".next").click(function() { plusSlides(1); });
+        
+        $(".dot").click(function() {
+            currentSlide($(this).index() + 1);
+        });
+        
+        $(".thumbnail").click(function() {
+            currentSlide($(this).index() + 1);
+        });
+        
+        // Pause on hover
+        $(".slideshow-container").hover(
+            function() { clearInterval(slideInterval); },
+            function() { startTimer(); }
+        );
+    }
+
+    // 4. Finally, initialize everything
+    initSlideshow();
+});
